1081522ea532fabb3a80988d6c63ea1b4000f352
Ozzy Cardenas <ocardenas789@me.com>
2018-02-28 12:32:05 -0600

    created database SQL file

---
+++ bamazon.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,4) NOT NULL,
+  stock_quantity INT default 0,
+  PRIMARY KEY (item_id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("YEEZY BLUSH 500", 'Shoes', 250.00, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("YEEZY BREDS 350", 'Shoes', 220.00, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NIKE OFF-WHITE AIR 1", 'Shoes', 300.00, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NIKE OFF-WHITE AIR 1", 'Shoes', 300.00, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("FEAR OF GOD YELLOW BOMBER", 'Jackets', 150.00, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NORTHFACE GORTEX RED JACKET", 'Jackets', 250.00, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("YELLOW SUN DRESS", 'Dresses', 250.00, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("RAY BAN AVIATORS", 'Sunglasses', 170.00, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("RAY BAN ORIGINALS", 'Sunglasses', 200.00, 1);
+
+
+
+SELECT * FROM products;
