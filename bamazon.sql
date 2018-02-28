d332b621b90cc9c0f532ab53fede232b19df5dc3
Ozzy Cardenas <ocardenas789@me.com>
2018-02-28 17:03:18 -0600

    add json pkgs and .gitignore, and complited logic for bamazonCustomer.js

--- bamazon.sql
+++ bamazon.sql
@@ -7,21 +7,24 @@ CREATE TABLE products(
   item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NOT NULL,
   department_name VARCHAR(100) NOT NULL,
-  price DECIMAL(10,4) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
   stock_quantity INT default 0,
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("YEEZY BLUSH 500", 'Shoes', 250.00, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("YEEZY BLUSH 500", 'Shoes', 250.00, 4);
 INSERT INTO products (product_name, department_name, price, stock_quantity) values ("YEEZY BREDS 350", 'Shoes', 220.00, 5);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NIKE OFF-WHITE AIR 1", 'Shoes', 300.00, 1);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NIKE OFF-WHITE AIR 1", 'Shoes', 300.00, 1);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("FEAR OF GOD YELLOW BOMBER", 'Jackets', 150.00, 1);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NORTHFACE GORTEX RED JACKET", 'Jackets', 250.00, 1);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("YELLOW SUN DRESS", 'Dresses', 250.00, 1);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("RAY BAN AVIATORS", 'Sunglasses', 170.00, 1);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ("RAY BAN ORIGINALS", 'Sunglasses', 200.00, 1);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NIKE OFF-WHITE AIR 1", 'Shoes', 300.00, 9);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NIKE OFF-WHITE AIR 1", 'Shoes', 300.00, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("FEAR OF GOD YELLOW BOMBER", 'Jackets', 150.00, 3);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("NORTHFACE GORTEX RED JACKET", 'Jackets', 250.00, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("YELLOW SUN DRESS", 'Dresses', 250.00, 4);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("RAY BAN AVIATORS", 'Sunglasses', 170.00, 9);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("RAY BAN ORIGINALS", 'Sunglasses', 200.00, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("APPLE WATCH", 'Technology', 300.00, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity) values ("iPhone X 64GB SILVER", 'Technology', 1000.00, 5);
 
 
 
 SELECT * FROM products;
+SELECT item_id, product_name, price  FROM products;
---
+++ bamazonCustomer.js
@@ -0,0 +1,123 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// connection to MySQL server
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  // console.log("connected as id " + connection.threadId);
+  currentInventory();
+});
+
+// prints current items for sale to list.
+function currentInventory(){
+  console.log("Here is a list of the current items we are selling...\n");
+  connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    for (var i = 0; i < res.length; i++) {
+      console.log("Product ID: " + res[i].item_id + " || Item: " + res[i].product_name + " || Price: $" + res[i].price);
+    };
+    console.log("\n")
+    userStart()
+  });
+
+};
+
+// Asks user what item they want to buy and how many
+function userStart(){
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the Product ID you would like to purchase?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many do you want to buy?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+
+      // Connects to server and finds items necessary
+      var query = "SELECT item_id,product_name, stock_quantity, price FROM products WHERE ?";
+      connection.query(query, { item_id: answer.item }, function(err, res) {
+        if (err) throw err;
+
+          // console.log(res[0]);
+          // If there isnt enough stock_quantity
+          if(res[0].stock_quantity < answer.quantity){
+          console.log("Insufficient quantity brah! \n" + "We ony have " + res[0].stock_quantity + " of these!");
+          userStart();
+          }
+          // When in stock, update server and check out customer
+          else {
+            var newQuantity = res[0].stock_quantity - answer.quantity;
+            console.log("Updated Quantity")
+            console.log(newQuantity);
+            connection.query(
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  stock_quantity: newQuantity
+                },
+                {
+                  item_id: answer.item
+                }
+              ],
+              function(error) {
+                if (error) throw err;
+                // What prints to console for checkout
+                console.log("Succesfully added to cart! \n");
+                var customerTotal = answer.quantity * res[0].price;
+                console.log("Your cart total is $" + customerTotal);
+                console.log("Whenever you're ready, throw $" + customerTotal + " in my pocket ;)")
+
+                // constinue??
+                inquirer
+                  .prompt({
+                    name: "cont",
+                    type: "confirm",
+                    message: "Want to Continue shopping?"
+                  })
+                  .then(function(answer) {
+
+                      if(answer.cont){
+                        currentInventory();
+                      } else {
+                        console.log("Okay then come back soon!")
+                        connection.end();
+                      }
+                  });
+              }
+            );
+          };
+       // run next function here
+      });
+    });
+};
